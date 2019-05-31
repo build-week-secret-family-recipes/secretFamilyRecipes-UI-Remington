36b32340e92f5326753eece5d5820eee858e8e02
codeasaglacier <remingtonshock@gmail.com>
2019-05-31 09:40:02 -0800

    final touches, more js

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const mouseoverRed = document.querySelectorAll('.content');
 mouseoverRed.forEach(element => {
     element.addEventListener('mouseover', event => {
         event.target.style.color = "#e3e3e3";
-        setTimeout(()=> event.target.style.color = "", 3000)
+        setTimeout(()=> event.target.style.color = "", 1500)
+    })
+})
+const mouseoverGrey= document.querySelectorAll('.recipes');
+mouseoverGrey.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.target.style.color = "#1f1f44";
+        setTimeout(()=> event.target.style.color = "", 1500)
+    })
+})
+const mouseoverCREAM= document.querySelectorAll('.contact');
+mouseoverCREAM.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.target.style.color = "#a00000";
+        setTimeout(()=> event.target.style.color = "", 1500)
     })
 })
